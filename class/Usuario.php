e5c5945c5173d57bc34c5da566b86f77108f636c
Brenno marques <brenno.lima@soluti.com.br>
2020-07-27 12:24:11 -0300

    Feito

--- class/Usuario.php
+++ class/Usuario.php
@@ -33,7 +33,7 @@
             $this->dtcadastro = $value;
         }
 
-        public function loadById($id){
+        public function loadById($id){//usuario por ID
             $sql = new Sql();
 
             $result = $sql->select("SELECT * FROM usuario WHERE idusuario = :ID", array(
@@ -41,15 +41,86 @@
             ));
 
             if(count($result) > 0){
-                $row = $result[0];
-                $this->setIdusario($row['idusuario']);
-                $this->setDeslogin($row['deslogin']);
-                $this->setDessenha($row['dessenha']);
-                $this->setDtcadastro(new DateTime($row['dtcadastro']));//já pasando para tipo timestempo
+                $this->setData($result[0]);
             }
         }
 
-        public function __toString(){
+        public static function eListAll(){ //lista todos os usuario
+            $sql = new Sql();
+            return $sql->select("SELECT * FROM usuario ORDER BY idusuario;");
+        }
+
+        public function login($login, $password){
+            $sql = new Sql();
+
+            $result = $sql->select("SELECT * FROM usuario WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+                ":LOGIN"=>$login,
+                ":PASSWORD"=>$password
+            ));
+
+            if(count($result) > 0){
+                $this->setData($result[0]);
+            }else{
+                throw new Exception("Login e/ou senha inválidos");
+                
+            }
+        }
+
+        public static function eSearch($login){//realiza busca no busca.
+            $sql = new Sql();
+            return $sql->select("SELECT * FROM usuario WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+                ':SEARCH'=>"%".$login."%"
+            ));
+
+        }
+
+        public function setData($data){//dados
+            $this->setIdusario($data['idusuario']);
+            $this->setDeslogin($data['deslogin']);
+            $this->setDessenha($data['dessenha']);
+            $this->setDtcadastro(new DateTime($data['dtcadastro']));//já pasando para tipo timestempo
+        }
+
+        public function eInsert(){//inserir dados
+            $sql = new Sql();
+            $results = $sql->select("CALL sp_usuario_insert(:LOGIN, :PASSWORD)", array(
+                ':LOGIN'=>$this->getDeslogin(),
+                ':PASSWORD'=>$this->getDessenha()
+            ));
+            if(count($results) > 0){
+                $this->setData($results[0]);
+            }
+        }
+
+        public function eUpdate($login, $password){//Atualizar
+            
+            $this->setDeslogin($login);
+            $this->setDessenha($password);
+
+            $sql = new Sql();
+
+            $sql->query("UPDATE usuario SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario =:ID", array(
+                'LOGIN'=>$this->getDeslogin(),
+                'PASSWORD'=>$this->getDessenha(),
+                'ID'=>$this->getIdusario()
+            ));
+        }
+
+        public function eDelete(){//Deletar usuario 
+
+            $sql = new Sql();
+
+            $sql->query("DELETE FROM usuario WHERE idusuario =:ID", array(
+                'ID'=>$this->getIdusario()
+            ));
+
+            $this->setIdusario("0");
+            $this->setDeslogin("");
+            $this->setDessenha("");
+            $this->setDtcadastro(new DateTime());
+        }    
+
+        public function __toString(){//exibir em formato Json
             return json_encode(array(
                 "idusuario"=>$this->getIdusario(),
                 "deslogin"=>$this->getDeslogin(),
--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <?php
 
     require_once("config.php");
+/*********************BUSCAS****************/
+    //Busca Por ID usuario
+    // $root = new Usuario();
+    // $root->loadById(1);
+    // echo $root;
 
-    $root = new Usuario();
+    //Busca todos Usuarios
+    // $lista = Usuario::eListAll();
+    // echo json_encode($lista);
 
-    $root->loadById(1);
+    //Busca determinado usuario
+    // $search = Usuario::eSearch("root");
+    // echo json_encode($search)
 
-    echo $root;
+    //Carregar um usuario usando login e a senha
+    // $usuario = new Usuario();
+    // $usuario->login("root", "1235");
+    // echo $usuario;
+/*********************INSERT****************/
+    // $aluno = new Usuario();
+    // $aluno->setDeslogin("joão");
+    // $aluno->setDessenha("@joaor");
+    // $aluno->eInsert();
+    // echo $aluno;
 
+/*********************UPDATE****************/
+// $usuario = new Usuario();
+// $usuario->loadById(11);
+// $usuario->eUpdate("vini", "1250");
+// echo $usuario;
+
+/*********************DELETE****************/
+    $usuario = new Usuario();
+    $usuario->loadById(8);
+    $usuario->eDelete();
+    echo $usuario;
 
 ?>
